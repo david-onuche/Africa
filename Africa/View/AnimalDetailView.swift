b7aa1c65385ff4fb42e00e26f8e8388bef9c2e3b
davidonuche <iamdavidonuche@gmail.com>
2026-09-19 14:18:01 +0100

    Done with detailed view for view for each animal with Dyamic data.

---
+++ Africa/View/AnimalDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  AnimalDetailView.swift
+//  Africa
+//
+//  Created by David Onuche on 19/09/2026.
+//
+
+import SwiftUI
+
+struct AnimalDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnimalDetailView()
+}
