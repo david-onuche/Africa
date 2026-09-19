a50989e6ebe0f9245c5143370a88be88771ba4f2
davidonuche <iamdavidonuche@gmail.com>
2026-09-19 12:17:45 +0100

    Add CoverImageView component with initial implementation

---
+++ Africa/View/CoverImageView.swift
@@ -0,0 +1,18 @@
+//
+//  CoverImageView.swift
+//  Africa
+//
+//  Created by David Onuche on 19/09/2026.
+//
+
+import SwiftUI
+
+struct CoverImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CoverImageView()
+}
