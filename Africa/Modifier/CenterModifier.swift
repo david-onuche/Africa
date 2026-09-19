df8bfa53de83b273221918b259dd905f6b031382
davidonuche <iamdavidonuche@gmail.com>
2026-09-20 00:52:23 +0100

    Done with the project

---
+++ Africa/Modifier/CenterModifier.swift
@@ -0,0 +1,18 @@
+//
+//  CenterModifier.swift
+//  Africa
+//
+//  Created by David Onuche on 20/09/2026.
+//
+
+import SwiftUI
+
+struct CenterModifier: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CenterModifier()
+}
---
+++ Africa/View/CreditsView.swift
@@ -0,0 +1,18 @@
+//
+//  CreditsView.swift
+//  Africa
+//
+//  Created by David Onuche on 20/09/2026.
+//
+
+import SwiftUI
+
+struct CreditsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreditsView()
+}
