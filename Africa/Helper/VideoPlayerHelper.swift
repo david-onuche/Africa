d70387eab9748a2f31798d30a36f9475cf8ce964
davidonuche <iamdavidonuche@gmail.com>
2026-09-19 21:01:39 +0100

    Map with custom annontation

---
+++ Africa/Helper/VideoPlayerHelper.swift
@@ -0,0 +1,18 @@
+//
+//  VideoPlayerHelper.swift
+//  Africa
+//
+//  Created by David Onuche on 19/09/2026.
+//
+
+import SwiftUI
+
+struct VideoPlayerHelper: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VideoPlayerHelper()
+}
---
+++ Africa/View/VideoPlayerView.swift
@@ -0,0 +1,18 @@
+//
+//  VideoPlayerView.swift
+//  Africa
+//
+//  Created by David Onuche on 19/09/2026.
+//
+
+import SwiftUI
+
+struct VideoPlayerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VideoPlayerView()
+}
